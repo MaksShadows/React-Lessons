988644e505e72a60ccfc86998e5599ac1aae34ba
MaksShadows <Lykanenko1995@gmail.com>
2020-04-14 20:02:20 +0300

    2

--- Lesson1/task2/index.js
+++ Lesson1/task2/index.js
@@ -1,16 +1,14 @@
 const element = React.createElement(
   'div', 
   { className: 'greeting' },
-   [
-      React.createElement(
-          'div', { className: 'greeting__title' },
-          'Hello, world!'
-      ),
-      React.createElement(
-          'div', { className: 'greeting__text' },
-          'I\'m learning React'
-      ),
-  ]
+  React.createElement(
+    'div', { className: 'greeting__title' },
+    'Hello, world!'
+),
+  React.createElement(
+    'div', { className: 'greeting__text' },
+    'I\'m learning React'
+)
 );
 
 const rootElem = document.querySelector('#root');
