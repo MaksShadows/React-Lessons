4eb72d1df49f46368a6c6a1e44b847c5f8bdf50e
Maks1501 <49039860+MaksShadows@users.noreply.github.com>
2020-04-17 20:59:24 +0300

    Update Profile.jsx

--- Lesson3/task5/src/Profile.jsx
+++ Lesson3/task5/src/Profile.jsx
@@ -4,11 +4,7 @@ import './profile.scss';
 
 
 const formatDate = date => moment(date).format("DD MMM YY");
-<<<<<<< HEAD
 
-
-=======
->>>>>>> 5f8d3c1b06ecc2c2e7e5b28467cdefa9ff7cd761
 
 const Profile = (props) => {
     return (
@@ -16,11 +12,8 @@ const Profile = (props) => {
             <div className="profile__name">
                 {`${props.userData.firstName} ${props.userData.lastName}`}
             </div>
-<<<<<<< HEAD
             <div className="profile__birth">
-=======
               <div className="profile__birth">
->>>>>>> 5f8d3c1b06ecc2c2e7e5b28467cdefa9ff7cd761
                 {`Was born ${formatDate(props.userData.birthDate)} in ${props.userData.birthPlace}`}
             </div>
         </>
