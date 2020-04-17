ea312683688b9ad1570ab76c2c4de7a721a49192
MaksShadows <Lykanenko1995@gmail.com>
2020-04-17 21:10:52 +0300

    4232

--- Lesson5/task6/src/ColorPicker.jsx
+++ Lesson5/task6/src/ColorPicker.jsx
@@ -1,53 +1,44 @@
-import React,  {Component}  from 'react';
+import React, { Component } from 'react';
 
 const CORAL = 'Coral';
 const AQUA = 'Aqua';
 const BISQUE = 'Bisque';
 
+class ColorPicker extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            currentColor: ""
+        }
+    }
+    hoverColor = (color) => {
+        this.setState({
+            currentColor: color
+        })
+    }
 
-class ColorPicker extends React.Component {
-
-   constructor(props) {
-     super(props);
-     this.state = {
-       color: ""
-     }
-   }
-
-    hoverColor = color => {
-      this.setState({
-        color:  color,
-      })
-   }
-   reset = () =>{
-     this.setState({
-       color: "",
-     })
-   }
-
-   render() {
-     return (
-      <div>
-      <div class="picker__title">
-        {this.state.color}
-      </div>
-      <div>
-        <button class="picker__button picker__button_coral"
-        onMouseEnter={this.hoverColor.bind(this, CORAL)}
-        onMouseLeave={this.reset}
-        ></button>
-        <button class="picker__button picker__button_aqua"
-            onMouseEnter={this.hoverColor.bind(this, AQUA)}
-            onMouseLeave={this.reset}
-        ></button>
-        <button class="picker__button picker__button_bisque"
-           onMouseEnter={this.hoverColor.bind(this, BISQUE)}
-           onMouseLeave={this.reset}></button>
-      </div>
-    </div>
-     );
-   }
-};
-
-
+    reset = () => {
+        this.setState({
+            currentColor: ""
+        })
+    }
+    render() {
+        return (
+            <div>
+                <div className="picker__title">{this.state.currentColor}</div>
+                <button className="picker__button picker__button_coral"
+                    onMouseEnter={this.hoverColor.bind(this, CORAL)}
+                    onMouseLeave={this.reset}
+                ></button>
+                <button className="picker__button picker__button_aqua"
+                    onMouseEnter={this.hoverColor.bind(this, AQUA)}
+                    onMouseLeave={this.reset}></button>
+                <button className="picker__button picker__button_bisque"
+                    onMouseEnter={this.hoverColor.bind(this, BISQUE)}
+                    onMouseLeave={this.reset}
+                ></button>
+            </div>
+        )
+    }
+}
 export default ColorPicker;
