d2ccd2c06af7be8b0eb35551b2dc16b5bf733d52
MaksShadows <Lykanenko1995@gmail.com>
2020-04-18 13:55:00 +0300

    task3

--- Lesson6/task3/src/index.jsx
+++ Lesson6/task3/src/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Page from './Page';
 import './index.scss';
 
 const rootElem = document.querySelector('#root');
 
-ReactDOM.render( rootElem);
+ReactDOM.render( <Page />,rootElem);
