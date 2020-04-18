ec3bf7da746c16a84462708a5db5b05a6ff4e9e4
MaksShadows <Lykanenko1995@gmail.com>
2020-04-18 15:00:02 +0300

    55

--- Lesson6/task5/src/Spinner.jsx
+++ Lesson6/task5/src/Spinner.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Spinner = ({ size }) => {
-    return (
-        <span className="spinner" style={{ width: size, height: size }}></span>
-    )
-}
-
+    const style = {
+      width: `${size}px`,
+      height: `${size}px`
+    };
+    return <span className="spinner" style={style}></span>;
+  };
 export default Spinner;
