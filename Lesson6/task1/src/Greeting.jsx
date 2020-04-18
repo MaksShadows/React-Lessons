11ad6c60e67a5a0e30ed432fda1e145db25be4db
MaksShadows <Lykanenko1995@gmail.com>
2020-04-18 13:24:34 +0300

    121

--- Lesson6/task1/src/Greeting.jsx
+++ Lesson6/task1/src/Greeting.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import UserGreeting from './UserGreeting';
 import GuestGreeting from './GuestGreeting';
 
-const Greeting = (props) => {
-    if (props.isLoggedIn) 
-        return <UserGreeting />;
-    
-    return <GuestGreeting />
-}
+const Greeting = ( {isLoggedIn} ) => {
+    if (isLoggedIn) {
+      return <UserGreeting />;
+    }
+    return <GuestGreeting />;
+  }
 
 export default Greeting;
