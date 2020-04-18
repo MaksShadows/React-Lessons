d8f3c4fd441ecdc5d909050f2b95fbc238a5d275
MaksShadows <Lykanenko1995@gmail.com>
2020-04-18 13:23:37 +0300

    111

--- Lesson6/task1/src/GuestGreeting.jsx
+++ Lesson6/task1/src/GuestGreeting.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 
 const GuestGreeting  = () => {
     return(
-<<<<<<< HEAD
       <h1>Hi stranger. Please login 🚀</h1>
-=======
-      <h1>Hi stranger. Please login :)</h1>
->>>>>>> dca69b0e21260c050501fec42560086ff4dd704b
     );
 };
 
