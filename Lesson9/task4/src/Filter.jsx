c130f2a5b15fb4b540bcb6fd4cb1d1885e6983e1
MaksShadows <Lykanenko1995@gmail.com>
2020-04-23 12:57:09 +0300

    task4

---
+++ Lesson9/task4/src/Filter.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Filter = (props) => {
+
+  const { count, filterText, onChange } = props;
+  return (
+      <div className="filter">
+          <span className="filter__count">{count}</span>
+          <input type="text" className="filter__input"
+              value={filterText}
+              onChange={onChange} />
+      </div>
+  )
+}
+
+export default Filter;
---
+++ Lesson9/task4/src/UsersList.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import User from "./User";
+import Filter from "./Filter";
+
+class UsersList extends Component {
+  constructor(props) {
+      super(props)
+
+      this.state = {
+          name: '',
+          userNames: props.users,
+      };
+
+  }
+      onChange = event => {
+          const { name, value } = event.target;
+
+          const arr = this.props.users.filter(user => user.name.toUpperCase().includes(value.toUpperCase()));
+          
+          this.setState({
+              [name]: value,
+              userNames: arr,
+          });
+      };
+
+     
+      render() {
+          return (
+              <div>
+                  <Filter count={this.state.userNames.length} 
+                          onChange={this.onChange}
+                          filterText={this.state.name}
+                  />
+                  <ul className="users">
+                      {this.state.userNames.map(({id, name, age}) => 
+                          <User key={id} name={name} age={age} />
+                      )}
+                  </ul>
+              </div>
+          );
+      }
+  }
+
+
+
+export default UsersList;
