6372dcadbc2eca8379f80dbbc7f0cd4a9460b688
MaksShadows <Lykanenko1995@gmail.com>
2020-04-24 12:39:43 +0300

    2

--- Lesson10/task2/src/App.jsx
+++ Lesson10/task2/src/App.jsx
@@ -1,39 +1,42 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import ShoppingCart from './ShoppingCart';
 import Profile from './Profile';
 
-
-
 class App extends Component {
+ 
+    state ={
+        userData: {
+            firstName: 'John',
+            lastName: 'Doe',
+        }  
+     };
+     
+     handleChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            userData: {
+               ...this.state.userData,
+               [name]: value
+            }   
+        });
+    };
+  
+    render() {
 
-  constructor(props) {
-      super(props);
-      this.state = {
-          firstName: "John",
-          lastName: "Doe"
-      }
-  };
-
-  handleChange = (event) => {
-      const { name, value } = event.target;
-      this.setState({
-          ...this.state,
-          [name]: value
-      });
-  };
-
-  render() {
-      const { userData } = this.state
-      return (
-          <div className="page">
-              <h1 className="title">Hello, {userData.firstName} {userData.lastName}</h1>
-              <main className="content">
-                  <ShoppingCart userName={userData.firstName} />
-                  <Profile userData={this.state} handleChange={this.handleChange} />
-              </main>
-          </div>
-      );
-  }
+        const { userData } = this.state;
+            return (
+            <div className="page">
+                <h1 className="title">{`Hello, ${userData.firstName} ${userData.lastName}`}</h1>
+                <main className="content">
+                    <ShoppingCart userName={userData.firstName}/>
+                    <Profile 
+                        userData={userData} 
+                        handleChange={this.handleChange}
+                    />
+                </main>
+            </div>
+        );
+    };
 }
 
 export default App;
